5fd73408dd3b4bb2682c581505d33f880e1bcd04
Shawn Chenxiao Li <shawn.li@sdce.com.au>
2019-02-26 15:15:29 +1100

    add getnewaddress

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shawn-cx-li/omnilayer-go
+
+require github.com/ibclabs/omnilayer-go v0.0.0-20190208134129-cfb55a22083b
---
+++ omnijson/getnewaddress.go
@@ -0,0 +1,20 @@
+package omnijson
+
+type GetNewAddressResult = struct {
+	Address  string `json:"address"`
+	Address2 string `json:""`
+}
+
+type GetNewAddressCommand struct{}
+
+func (GetNewAddressCommand) Method() string {
+	return "getnewaddress"
+}
+
+func (GetNewAddressCommand) ID() string {
+	return "1"
+}
+
+func (GetNewAddressCommand) Params() []interface{} {
+	return nil
+}
